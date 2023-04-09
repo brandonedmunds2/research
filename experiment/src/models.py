404b01102b702aac76fc48e778c3e4bd174a7c58
Brandon Edmunds <brandonedmunds2@gmail.com>
2023-04-09 13:12:06 -0400

    .

--- experiment/src/models.py
+++ experiment/src/models.py
@@ -6,8 +6,8 @@ class SimpleNet(nn.Module):
         super().__init__()
         self.num_features=num_features
         self.num_classes=num_classes
-        self.fc1=nn.Linear(in_features=num_features,out_features=16)
-        self.fc2=nn.Linear(in_features=16,out_features=num_classes)
+        self.fc1=nn.Linear(in_features=num_features,out_features=256)
+        self.fc2=nn.Linear(in_features=256,out_features=num_classes)
     def forward(self,x):
         pred=F.relu(self.fc1(x.view(-1,self.num_features)))
         pred=self.fc2(pred)
