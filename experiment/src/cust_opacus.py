2ad3cddb8f1599f9c700669ffa0d7c142afd0108
Brandon Edmunds <brandonedmunds2@gmail.com>
2023-04-19 08:24:50 -0400

    fix roll

--- experiment/src/cust_opacus.py
+++ experiment/src/cust_opacus.py
@@ -6,7 +6,13 @@ from opacus.optimizers.optimizer import _check_processed_flag, _mark_as_processe
 from typing import List, Union
 from torch.nn.utils.prune import _compute_nparams_toprune, _validate_pruning_amount
 
-def compute_mask(t,amount,strategy,prev_mask):
+class MaskedDPOptimizer(DPOptimizer):
+    def __init__(self, amount,strategy, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.amount=amount
+        self.strategy=strategy
+        self.prev_mask={}
+    def compute_mask(self,t,amount,strategy,param_ind):
         default_mask=torch.ones_like(t)
         tensor_size = t.nelement()
         nparams_toprune = _compute_nparams_toprune(amount, tensor_size)
@@ -21,23 +27,18 @@ def compute_mask(t,amount,strategy,prev_mask):
             elif(strategy=='magnitude reverse'):
                 topk = torch.topk(torch.abs(t).view(-1), k=nparams_toprune, largest=True)
             elif(strategy=='rolling'):
-                if(prev_mask == None):
+                if(param_ind not in self.prev_mask):
                     prob = torch.rand_like(t)
                     topk = torch.topk(prob.view(-1), k=nparams_toprune)
+                    mask.view(-1)[topk.indices] = 0
                 else:
-                    mask=torch.roll(prev_mask,1)
-                    return mask
+                    mask=torch.roll(self.prev_mask[param_ind],1)
+                self.prev_mask[param_ind]=mask
+                return mask
             mask.view(-1)[topk.indices] = 0
         return mask
-
-class MaskedDPOptimizer(DPOptimizer):
-    def __init__(self, amount,strategy, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.amount=amount
-        self.strategy=strategy
-        self.prev_mask=None
     def add_noise(self):
-        for p in self.params:
+        for i,p in enumerate(self.params):
             _check_processed_flag(p.summed_grad)
 
             noise = _generate_noise(
@@ -46,7 +47,7 @@ class MaskedDPOptimizer(DPOptimizer):
                 generator=self.generator,
                 secure_mode=self.secure_mode,
             )
-            mask=compute_mask(p,amount=self.amount,strategy=self.strategy,prev_mask=self.prev_mask)
+            mask=self.compute_mask(p,amount=self.amount,strategy=self.strategy,param_ind=i)
             noise.mul_(mask)
             p.grad = (p.summed_grad + noise).view_as(p)
 
